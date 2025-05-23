92a4236214807a6540c77dfcd273f38cda25a4cc
Noah Langensee <noah.langensee@sluz.ch>
2025-05-23 15:45:08 +0200

    car simulation done

--- src/parking-lot.ts
+++ src/parking-lot.ts
@@ -1,14 +1,39 @@
-export class ParkingLot {
+export interface Subscriber {
+  update(lot: ParkingLot, type: 'enter' | 'exit'): void;
+}
+export interface Publisher {
+  subscribe(subscriber: Subscriber): void;
+  unsubscribe(subscriber: Subscriber): void;
+  notify(type: 'enter' | 'exit'): void;
+}
+
+export class ParkingLot implements Publisher {
   public occupied: number = 0;
+  private subscribers: Subscriber[] = [];
 
   constructor(
     public name: string,
     public capacity: number,
   ) {}
 
+  subscribe(subscriber: Subscriber): void {
+    if (!this.subscribers.includes(subscriber)) {
+      this.subscribers.push(subscriber);
+    }
+  }
+
+  unsubscribe(subscriber: Subscriber): void {
+    this.subscribers = this.subscribers.filter((s) => s !== subscriber);
+  }
+
+  notify(type: 'enter' | 'exit'): void {
+    this.subscribers.forEach((subscriber) => subscriber.update(this, type));
+  }
+
   enter() {
     if (!this.isFull()) {
       this.occupied++;
+      this.notify('enter');
     } else {
       throw new Error(`the parking lot is full`);
     }
@@ -17,6 +42,7 @@ export class ParkingLot {
   exit() {
     if (!this.isEmpty()) {
       this.occupied--;
+      this.notify('exit');
     } else {
       throw new Error(`the parking lot is empty`);
     }
@@ -28,5 +54,16 @@ export class ParkingLot {
 
   isEmpty() {
     return this.occupied == 0;
+  }
+}
+
+export class Display implements Subscriber {
+  update(lot: ParkingLot, type: 'enter' | 'exit'): void {
+    const message =
+      type === 'enter'
+        ? `A car entered the lot ${lot.name}: ${lot.occupied}/${lot.capacity} occupied.`
+        : `A car left the lot ${lot.name}: ${lot.occupied}/${lot.capacity} occupied.`;
+
+    console.log(message);
   }
 }
